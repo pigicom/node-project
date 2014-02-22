8bb6922fa99afe8b64518e0ff0ff40e68fbfad88
pigicom <pigicom@gmail.com>
2014-02-22 17:11:56 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,12 @@
-node socket-server.js
+var express = require('express');
+ 
+var app = express.createServer(express.logger());
+ 
+app.get('/', function(request, response) {
+  response.send('hello, world');
+});
+ 
+var port = process.env.PORT || 3000;
+console.log("Listening on " + port);
+ 
+app.listen(port);
